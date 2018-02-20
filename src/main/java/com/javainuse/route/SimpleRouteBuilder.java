b308059ab982053f3730d1a2d3d34042ca10953a
lucasmaryn <lucasmaryn@interia.pl>
2018-02-20 06:53:45 +0100

    camel, activemq - EIP - Recipient List

--- src/main/java/com/javainuse/route/SimpleRouteBuilder.java
+++ src/main/java/com/javainuse/route/SimpleRouteBuilder.java
@@ -1,22 +1,23 @@
 package com.javainuse.route;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 public class SimpleRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
         from("file:C:/camel-InputFolder").split().tokenize("\n").to("direct:test");
 
-//Content Based routing- Route the message based on the token it contains.
-        from("direct:test").
-                choice().
-                when(body().contains("javainuse1"))
-                .to("jms:queue:javainuse1").
-                when(body().contains("javainuse2"))
-                .to("jms:queue:javainuse2")
-                .when(body().contains("javainuse3"))
-                .to("jms:queue:javainuse3").
-                otherwise().
-                to("jms:queue:otherwise");
+        //Recipient List- Dynamically set the recipients of the exchange
+        //by creating the queue name at runtime
+        from("direct:test")
+                .process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        String recipient = exchange.getIn().getBody().toString();
+                        String recipientQueue="jms:queue:"+recipient;
+                        exchange.getIn().setHeader("queue", recipientQueue);
+                    }
+                }).recipientList(header("queue"));
 
     }
 }
