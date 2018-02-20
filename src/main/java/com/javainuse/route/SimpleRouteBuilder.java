f52747c1af755f296e5a0d2c99f3051b38b3c410
lucasmaryn <lucasmaryn@interia.pl>
2018-02-20 06:48:37 +0100

    camel, activemq - EIP - content based router

--- src/main/java/com/javainuse/route/SimpleRouteBuilder.java
+++ src/main/java/com/javainuse/route/SimpleRouteBuilder.java
@@ -4,6 +4,19 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class SimpleRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
-        from("file:C:/camel-InputFolder").split().tokenize("\n").to("jms:queue:javainuse");
+        from("file:C:/camel-InputFolder").split().tokenize("\n").to("direct:test");
+
+//Content Based routing- Route the message based on the token it contains.
+        from("direct:test").
+                choice().
+                when(body().contains("javainuse1"))
+                .to("jms:queue:javainuse1").
+                when(body().contains("javainuse2"))
+                .to("jms:queue:javainuse2")
+                .when(body().contains("javainuse3"))
+                .to("jms:queue:javainuse3").
+                otherwise().
+                to("jms:queue:otherwise");
+
     }
 }
